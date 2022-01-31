7c8752397a8bb18df8b5faebe48d74e62b96eff9
farkon00 <sammer2016sammer@gmail.com>
2022-01-31 21:57:29 +0200

    V0.3 Functions added, refactoring done, code was sepearated in modules.

--- keywords/func_keyw.py
+++ keywords/func_keyw.py
@@ -5,6 +5,28 @@ from Function import Function
 
 from .check_args import check_args
 
+def func_keyword(lexic : list[str], i : int, code : str, in_vars : dict[str : int], block_indexes : list[tuple[int, int]]):
+    parts = " ".join(lexic).split(":")
+
+    func_name = parts[0].split()[1]
+    args = parts[1].split()
+
+    func_index = len("\n".join(code.split("\n")[:i+1]))-1
+
+    # Finding nearest block for tha function
+    block = block_indexes[0]
+    for j in block_indexes:
+        if func_index > j[0]:
+            if j[0] < block[0]:
+                block = j
+
+    if not code[func_index:block[1]].split():
+        raise SyntaxError(f"Between nearest block and function declaration code was found. Line : {i+1}")
+
+    func_code = code[block[0]:block[1]]
+
+    in_vars[func_name] = Function(args, func_code)
+
 def call_keyword(lexic : list[str], i : int, full_vars : dict[str : int]):
     args = lexic[2:]
     check_args(args, i)
