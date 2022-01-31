42cc13609badb244db27e7fa5da3542aee1a49a8
farkon00 <sammer2016sammer@gmail.com>
2022-01-31 18:47:26 +0200

    func, return, call keywords added

--- binarian.py
+++ binarian.py
@@ -1,30 +1,50 @@
 from dataclasses import dataclass
 import sys
-from types import NoneType
 
 global vars
+global block_indexes
+global code
 vars = {}
+code : str = None
+block_indexes = []
 
 @dataclass
 class Function:
     args : list[str]
-    lines : list[list[str]] # List contains lexic lists
+    lines : str
 
-    def execute(self):
-        pass
+    def execute(self, args, i):
+        local = {self.args[j] : args[j] for j in range(len(args))}
+        for line in map(lambda x : x.lower(), self.lines.split("\n")[1:-1]):
+            while "{" in line:
+                if line.count("{") == line.count("}"):
+                    line = expr_read(line, i, local=local)
+                else:
+                    raise SyntaxError('Expression must have start and finish matched with "{" and "}". Line : ' + str(i + 1))
+            
+            lexic = line.split()
+            if len(lexic) <= 0:
+                continue
 
-def execute_line(lexic : list[str], i : int, local : list[int | Function] | NoneType = None, is_expr=False) -> int:
+            ret = execute_line(lexic, i, local=local)
+
+            if ret != None:
+                return ret
+
+def execute_line(lexic : list[str], i : int, local : dict[str : Function] = None, is_expr=False) -> int | None:
     global vars
+    global code
 
-    full_vars = vars
+    full_vars = vars.copy()
     is_func = local != None
 
     if is_func:
-        full_vars += local
+        full_vars |= local
 
     for j in range(len(lexic)):
-        if lexic[j] in vars.keys():
-            lexic[j] = str(full_vars[lexic[j]])
+        if lexic[j] in full_vars.keys():
+            if not isinstance(full_vars[lexic[j]], Function):
+                lexic[j] = str(full_vars[lexic[j]])
 
     match lexic[0]:
         case "set":
@@ -78,48 +98,114 @@ def execute_line(lexic : list[str], i : int, local : list[int | Function] | None
             if is_expr:
                 raise SyntaxError(f"This operation is unavailable in expressions. Line : {i + 1}")
 
+            check_args((lexic[1]), i)
+
             # Error handeling
             if len(lexic) < 3:
                 raise SyntaxError(f"You didn`t give enough arguments. Line : {i + 1}")
-            if lexic[1] not in ("0", "1"):
-                raise NameError(f"Variable is not found. Line : {i + 1}")
 
             print(f"{lexic[2]} : {int(lexic[1])}")
 
 
         case "and":
-            check_args(lexic[0], i, arg2=lexic[1])
+            if len(lexic) <= 2:
+                raise SyntaxError(f"You didn`t give enough arguments. Line : {i + 1}")
+
+            check_args((lexic[1], lexic[2]), i)
 
             if not is_expr:
                 print(f"AND output : {int(int(lexic[1]) and int(lexic[2]))}. Line : {i + 1}")
 
-            return int(int(lexic[1]) and int(lexic[2]))
+            if is_expr:
+                return int(int(lexic[1]) and int(lexic[2]))
 
         case "or":
-            check_args(lexic[0], i, arg2=lexic[1])
+            if len(lexic) <= 2:
+                raise SyntaxError(f"You didn`t give enough arguments. Line : {i + 1}")
+
+            check_args((lexic[1], lexic[2]), i)
 
             if not is_expr:
                 print(f"OR output : {int(int(lexic[1]) or int(lexic[2]))}. Line : {i + 1}")
 
-            return int(int(lexic[1]) or int(lexic[2]))
+            if is_expr:
+                return int(int(lexic[1]) or int(lexic[2]))
 
         case "not":
-            check_args(lexic[0], i)
+            if len(lexic) <= 1:
+                raise SyntaxError(f"You didn`t give enough arguments. Line : {i + 1}")
+
+            check_args((lexic[1]), i)
             
             if not is_expr:
                 print(f"NOT output : {int(not int(lexic[1]))}. Line : {i + 1}")
 
-            return int(not int(lexic[1]))
+            if is_expr:
+                return int(not int(lexic[1]))
+
+        case "func":
+            parts = " ".join(lexic).split(":")
+
+            func_name = parts[0].split()[1]
+            args = parts[1].split()
+
+            func_index = len("\n".join(code.split("\n")[:i+1]))-1
+
+            # Finding nearest block for tha function
+            block = block_indexes[0]
+            for j in block_indexes:
+                if func_index > j[0]:
+                    if j[0] < block[0]:
+                        block = j
+
+            if not code[func_index:block[1]].split():
+                raise SyntaxError(f"Between nearest block and function declaration code was found. Line : {i+1}")
+
+            func_code = code[block[0]:block[1]]
+
+            if is_func:
+                local.append(Function(args, func_code))
+
+            vars[func_name] = Function(args, func_code)
+        
+        case "call":
+            args = lexic[2:]
+            check_args(args, i)
+
+            # Error handeling
+            if lexic[1] in full_vars.keys():
+                if isinstance(full_vars[lexic[1]], Function):
+                    if len(full_vars[lexic[1]].args) != len(args):
+                        raise SyntaxError(f"You didn`t give enough arguments. Line : {i + 1}")
+                else:
+                    raise TypeError(f"This object is not callable. Line : {i + 1}")
+            else:
+                raise NameError(f"Function is not found. Line : {i + 1}")
+
+            ret = full_vars[lexic[1]].execute(args, i)
+
+            return ret if ret else 0
+
+        case "return":
+            check_args((lexic[1]), i)
+
+            # Error handeling
+            if not is_func:
+                raise SyntaxError(f'Keyword "return" is restricted out of functions. Line : {i + 1}')
+
+            return int(lexic[1])
 
 
         case _:
             raise NameError(f"Keyword did not found. Line : {i + 1}")
 
-def check_args(arg1 : str, i : int, arg2="0") -> None:
-    if arg1 not in ("0", "1") and arg2 not in ("0", "1"):
+
+
+def check_args(args : tuple[str], i : int) -> None:
+    if any(map(lambda x : x not in ("0", "1"), args)):
         raise NameError(f"Variable is not found. Line : {i + 1}")
 
-def expr_read(line : str, i : int) -> str:
+def expr_read(line : str, i : int, local : dict[str : Function] = None) -> str:
     global vars
 
     end_ind = line.find("}")
@@ -128,45 +214,83 @@ def expr_read(line : str, i : int) -> str:
             if line[j] == "{":
                 start_ind = j
                 break
+        else: # If for ends without break. That means "}" located before "{"
+            raise SyntaxError('Expression must have start and finish matched with "{" and "}". Line : ' + str(i + 1))
 
     lexic = line[start_ind+1:end_ind].split()
-
-    for j in range(len(lexic)):
-        if lexic[j] in vars.keys():
-            lexic[j] = str(vars[lexic[j]])
             
-    return line.replace(line[start_ind:end_ind] + "}", str(execute_line(lexic, i, is_expr=True)))
+    return line.replace(line[start_ind:end_ind] + "}", str(execute_line(lexic, i, is_expr=True, local=local)))
+
+def main():
+    global vars
+    global block_indexes
+    global code
+
+    try:
+        code = open(sys.argv[1], "r", encoding="utf-8").read()
+    except:
+        code = open("test.bino", "r", encoding="utf-8").read()
+        #raise FileExistsError("File does not exist.")
+
+    while "//" in code:
+        comm_start = code.find("//")
+
+        if comm_start != -1:
+            code = code[:comm_start]
+
+    if "*" in code:
+        raise UnicodeEncodeError('Character "*" is restricted in binarian code')
+
+    # Blocks indexes search and deleting non-global scope code from executable
+    temp = code
+    executable_code = code
+    deleted = 0
+
+    if code.count("(") != code.count(")"):
+        raise SyntaxError('Functions must have start and finish matched with "(" and ")".')
+
+    while "(" in temp:
+        end_ind = temp.find(")")
+        if end_ind != -1:
+            start_ind = temp[:end_ind].rfind("(")
+
+            if start_ind == -1:
+                raise SyntaxError('Functions must have start and finish matched with "(" and ")".')
+
+            temp = temp[:start_ind]  + "{" + temp[start_ind+1:]
+            temp = temp[:end_ind]  + "}" + temp[end_ind+1:]
+
+            new_line_count = executable_code[start_ind:end_ind].count("\n")
+
+            executable_code = executable_code[:start_ind] + "*" * (end_ind - start_ind + 1) + executable_code[end_ind+1:]
+            executable_code = executable_code[:end_ind] + "\n" * new_line_count + executable_code[end_ind:]
+
+            block_indexes.append((start_ind, end_ind))  
+
+    executable_code = executable_code.replace("*", "")      
+
+    for i in range(len(executable_code.split("\n"))):
+
+        line = executable_code.split("\n")[i].lower()
+
+        while "{" in line:
+            if line.count("{") == line.count("}"):
+                line = expr_read(line, i)
+            else:
+                raise SyntaxError('Expression must have start and finish matched with "{" and "}". Line : ' + str(i + 1))
+        
+        lexic = line.split()
+        if len(lexic) <= 0:
+            continue
+
+        execute_line(lexic, i)
 
 
-try:
-    code = open(sys.argv[1], "r", encoding="utf-8").read()
-except:
-    code = open("test.bino", "r", encoding="utf-8").read()
-    #raise FileExistsError("File does not exist.")
+    try:
+        if sys.argv[2] == "debug":
+            print("\n" + str(vars), "\n\nFinished")
+    except:
+        print("\nFinished")
 
-for i in range(len(code.split("\n"))):
-    line = code.split("\n")[i].lower()
-
-    comm_start = line.find("//")
-
-    if comm_start != -1:
-        line = line[:comm_start]
-
-    while "{" in line:
-        if line.count("{") == line.count("}"):
-            line = expr_read(line, i)
-        else:
-            raise SyntaxError('Expression must have start and finish matched with "{" and "}". Line : ' + str(i + 1))
-    
-    lexic = line.split()
-    if len(lexic) <= 0:
-        continue
-
-    execute_line(lexic, i)
-
-
-try:
-    if sys.argv[2] == "debug":
-        print("\n" + str(vars), "\n\nFinished")
-except:
-    print("Finished")
+if __name__ == "__main__":
+    main()
