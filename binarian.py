6790550224420b29b75d76e29e9b248598e7eb35
farkon00 <sammer2016sammer@gmail.com>
2022-02-04 18:00:30 +0200

    Blocks parsing changed(bugs)

--- binarian.py
+++ binarian.py
@@ -1,6 +1,7 @@
-import sys
+from sys import argv
 
-from code_preparer import prepare_code
+from code_preparer import *
+from blocks_parser import *
 
 from keywords.log_oper import *
 from keywords.set_keyw import *
@@ -9,106 +10,119 @@ from keywords.func_keyw import *
 
 from Function import Function
 
-global block_indexes
-global code
 vars = {}
-code : str = None
-block_indexes = []
 
-def execute_line(lexic : list[str], i : int, local : dict[str : Function] = None, is_expr=False) -> int | None:
+code : str = ""
 
-    full_vars = vars.copy()
+def execute_line(lexic : list[str], i : int, opened_blocks : int, allowed_blocks : int, local : dict[str : Function] = None, is_expr=False) -> tuple[int, int, int] | tuple[int, int]:
+
+    if opened_blocks > allowed_blocks:
+        opened_blocks, allowed_blocks = parse_blocks(" ".join(lexic), opened_blocks, allowed_blocks)
+        return opened_blocks, allowed_blocks
+
     is_func = local != None
-
-    if is_func:
-        full_vars |= local
+    full_vars = {**vars, **(local if is_func else {})}
 
     for j in range(len(lexic)):
         if lexic[j] in full_vars.keys():
             if not isinstance(full_vars[lexic[j]], Function):
                 lexic[j] = str(full_vars[lexic[j]])
 
+    opened_blocks, allowed_blocks = parse_blocks(" ".join(lexic), opened_blocks, allowed_blocks)
+
+    if is_func and opened_blocks < 0:
+        opened_blocks = allowed_blocks = 0
+        return opened_blocks, allowed_blocks
+
     match lexic[0]:
         case "set":
             set_keyword(lexic, i, local if is_func else vars, is_expr, is_func)
+            return opened_blocks, allowed_blocks
 
         case "input":
             input_keyword(lexic, i, local if is_func else vars, is_expr, is_func)
+            return opened_blocks, allowed_blocks
 
         case "output":
             output_keyword(lexic, i, is_expr)
+            return opened_blocks, allowed_blocks
 
         case "and":
-            return and_keyword(lexic, i, is_expr)
+            return opened_blocks, allowed_blocks, and_keyword(lexic, i, is_expr)
 
         case "or":
-            return or_keyword(lexic, i, is_expr)
+            return opened_blocks, allowed_blocks, or_keyword(lexic, i, is_expr)
 
         case "not":
-            return not_keyword(lexic, i, is_expr)
+            return opened_blocks, allowed_blocks, not_keyword(lexic, i, is_expr)
 
         case "func":
-            if is_expr:
-                raise SyntaxError(f"This operation is unavailable in expressions. Line : {i + 1}")
-
-            func_keyword(lexic, i, code, local if is_func else vars, block_indexes)
+            func_keyword(lexic, i, code, local if is_func else vars, is_expr)
+            return opened_blocks, allowed_blocks
         
         case "call":
-            return call_keyword(lexic, i, full_vars)
+            opened_blocks += 1
+            allowed_blocks += 1
+            return call_keyword(lexic, i, code, opened_blocks, allowed_blocks, full_vars)
 
         case "return":
-            return return_keyword(lexic, i, is_func)
+            return opened_blocks, allowed_blocks, return_keyword(lexic, i, is_func)
 
 
         case _:
             raise NameError(f"Keyword did not found. Line : {i + 1}")
 
 
-def expr_read(line : str, i : int, local : dict[str : Function] = None) -> str:
+def execute_expr(line : str, i : int, opened_blocks : int, allowed_blocks : int, local : dict[str : Function] = None) -> str:
     global vars
 
-    end_ind = line.find("}")
-    if end_ind != -1:
-        for j in range(end_ind, 0, -1):
-            if line[j] == "{":
-                start_ind = j
-                break
-        else: # If for ends without break. That means "}" located before "{"
-            raise SyntaxError('Expression must have start and finish matched with "{" and "}". Line : ' + str(i + 1))
+    indexes = parse_expr(line, i)
+
+    if not indexes:
+        return line
+
+    start_ind, end_ind = indexes
 
     lexic = line[start_ind+1:end_ind].split()
-            
-    return line.replace(line[start_ind:end_ind] + "}", str(execute_line(lexic, i, is_expr=True, local=local)))
+
+    opened_blocks, allowed_blocks, replacement = execute_line(lexic, i, opened_blocks, allowed_blocks, is_expr=True, local=local)
+
+    return opened_blocks, allowed_blocks, line.replace(line[start_ind:end_ind] + "}",
+    str(replacement))
 
 def main():
-    global block_indexes
     global code
 
     try:
-        code = open(sys.argv[1], "r", encoding="utf-8").read()
+        code = open(argv[1], "r", encoding="utf-8").read().lower()
     except:
         raise FileExistsError("File does not exist.")
 
-    code, executable_code, block_indexes = prepare_code(code)
+    #code, executable_code, block_indexes = prepare_code(code) # TODO : remove this after implomenting new brackets parsing algorithm 
+    code = delete_comments(code)
+    opened_blocks = allowed_blocks = 0
 
-    for i in range(len(executable_code.split("\n"))):
+    for i in range(len(code.split("\n"))):
 
-        line = executable_code.split("\n")[i].lower()
+        line = code.split("\n")[i]
 
-        while "{" in line:
-            if line.count("{") == line.count("}"):
-                line = expr_read(line, i)
-            else:
-                raise SyntaxError('Expression must have start and finish matched with "{" and "}". Line : ' + str(i + 1))
+        # Expressions executing
+        if line.count("{") != line.count("}"):
+            raise SyntaxError('Expression must have start and finish matched with "{" and "}". Line : ' + str(i + 1))
+
+        if opened_blocks <= allowed_blocks:
+            while "{" in line:
+                opened_blocks, allowed_blocks, line = execute_expr(line, i, opened_blocks, allowed_blocks)
+            
         
         lexic = line.split()
         if len(lexic) <= 0:
             continue
 
-        execute_line(lexic, i)
+        opened_blocks, allowed_blocks = execute_line(lexic, i, opened_blocks, allowed_blocks)[:2]
 
 
-    if "-d" in sys.argv:
+    if "-d" in argv:
         print("\n" + str(vars))
 
     print("\nFinished")
