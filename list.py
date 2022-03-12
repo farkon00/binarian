08d83f16d373e15d8d4af863762b48a180fbad75
farkon00 <sammer2016sammer@gmail.com>
2022-03-12 09:01:06 +0200

    Index keyword added

--- list.py
+++ list.py
@@ -2,3 +2,7 @@ class List(list):
     """List in binarian"""
     def __str__(self):
         return f"[{' '.join([str(i) for i in self])}]"
+
+    def __int__(self):
+        """Converts binarian list from binary to decimal int"""
+        return sum([2 ** j * i for j, i in enumerate(self[::-1])])
