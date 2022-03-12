2bc3425db0597223702f1917167e1d455facf57b
farkon00 <sammer2016sammer@gmail.com>
2022-03-12 09:10:23 +0200

    Len keyword added

--- keywords/lists_keyw.py
+++ keywords/lists_keyw.py
@@ -14,3 +14,15 @@ def index_keyword(lexic : list[str], i : int, state, full_vars : dict[str : int]
         print(f"INDEX output : {list[index]}. Line : {i + 1}")
     else:
         return list[index]
+
+def len_keyword(lexic : list[str], i : int, state, full_vars : dict[str : int]):
+    if len(lexic) <= 1:
+        raise SyntaxError(f"You didn`t give enough arguments. Line : {i + 1}")
+
+    _list = get_var(lexic[1], i, full_vars, List)
+    _len = List(list(bin(len(_list)).replace('0b', '')))
+
+    if not state.is_expr:
+        print(f"LEN output : {_len}. Line : {i + 1}")
+    else:
+        return _len
