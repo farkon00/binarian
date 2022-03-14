8c6dc15aa1b04947c7cc014424ebfa4eab082e2d
farkon00 <sammer2016sammer@gmail.com>
2022-03-14 22:33:23 +0200

    Tests adapted for testing on local computer

--- run_tests.py
+++ run_tests.py
@@ -24,22 +24,22 @@ for test in tests:
 
 @pytest.mark.parametrize(["name", "argv"], tests)
 def test_language(name, argv):
-    sys.stdout = open(f"/home/runner/work/binarian/binarian/tests_results/{name}.txt", "w")
+    sys.stdout = open(f"tests_results/{name}.txt", "w")
 
     try:
-        sys.stdin = open(f"/home/runner/work/binarian/binarian/tests/{name}_input.txt", "r")
+        sys.stdin = open(f"tests/{name}_input.txt", "r")
     except FileNotFoundError:
         pass
     
     try:
-        main(test_argv=["binarian.py", f"/home/runner/work/binarian/binarian/tests/{name}.bino", *argv])
+        main(test_argv=["binarian.py", f"tests/{name}.bino", *argv])
     except SystemExit:
         pass
 
     sys.stdout.close()
 
-    result = open(f"/home/runner/work/binarian/binarian/tests_results/{name}.txt", "r").read()
-    expected = open(f"/home/runner/work/binarian/binarian/tests/expected_results/{name}.txt", "r").read()
+    result = open(f"tests_results/{name}.txt", "r").read()
+    expected = open(f"tests/expected_results/{name}.txt", "r").read()
 
     cut_index = result.rfind("Finished in")
     if cut_index != -1:
