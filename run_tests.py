7432e8aa05aa7162e32ee5cfd3306ca0e2769ebc
farkon00 <sammer2016sammer@gmail.com>
2022-03-14 18:09:07 +0200

    Everything seted up for testing

---
+++ run_tests.py
@@ -0,0 +1,34 @@
+import sys
+from binarian import main
+import pytest
+
+tests = [
+    ["for"]
+]
+
+# Adds argv everywhere
+for test in tests:
+    if len(test) == 1:
+        test.append([])
+
+@pytest.mark.parametrize(["name", "argv"], tests)
+def test_language(name, argv):
+    sys.stdout = open(f"tests_results/{name}.txt", "w")
+
+    try:
+        sys.stdin = open(f"tests/input_{name}.txt", "r")
+    except FileNotFoundError:
+        pass
+
+    main(test_argv=["binarian.py", f"tests/{name}.bino", *argv])
+
+    sys.stdout.close()
+
+    result = open(f"tests_results/{name}.txt", "r").read()
+    expected = open(f"tests/expected_results/{name}.txt", "r").read()
+
+    cut_index = result.rfind("Finished in")
+    if cut_index != -1:
+        result = result[:cut_index-2]
+
+    assert result == expected
