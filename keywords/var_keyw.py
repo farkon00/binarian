097691458b39c57f08dbe615fbb435365859a531
farkon00 <sammer2016sammer@gmail.com>
2022-03-20 18:16:50 +0200

    Tests fixed

--- keywords/var_keyw.py
+++ keywords/var_keyw.py
@@ -5,13 +5,14 @@ def set_keyword(lexic : list[str], state, in_vars : dict[str : object], full_var
     # Error handeling
     binarian_assert(state.is_expr, "This operation is unavailable in expressions.", state)
     binarian_assert(len(lexic) < 3, "You didn`t give enough arguments.", state)
-    binarian_assert(lexic[1] in state.RESTRICTED_NAMES, "Variable name is unavailable.", state)
 
     if len(lexic) >= 4:
         binarian_assert(lexic[1] not in state.types, f"Type is not found : {lexic[1]}", state)
+        binarian_assert(lexic[2] in state.RESTRICTED_NAMES, "Variable name is unavailable.", state)
         in_vars[lexic[2]] = get_var(lexic[3], full_vars, state)
         return
     
+    binarian_assert(lexic[1] in state.RESTRICTED_NAMES, "Variable name is unavailable.", state)
     in_vars[lexic[1]] = get_var(lexic[2], full_vars, state)
 
 def drop_keyword(lexic : list[str], state, in_vars : dict[str : object]):
