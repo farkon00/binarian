b0249a7e41fc4daf7e43269a93b7fe322727b6cb
farkon00 <sammer2016sammer@gmail.com>
2022-04-15 16:43:44 +0300

    floats added to typechecking

--- type_checking/get_type.py
+++ type_checking/get_type.py
@@ -3,9 +3,13 @@ from bin_types.list import List
 def get_type(var, state, full_vars):
     if var[0] == "[":
         return List
-    if var in state.types:
+    elif var.isdigit() or (var[0] == "-" and var[1:].isdigit()):
+        return int
+    elif var.replace(".", "").isdigit() or (var[0] == "-" and var[1:].replace(".", "").isdigit()):
+        return float
+    elif var in state.types:
         return state.types[var]
-    if var not in full_vars:
+    elif var not in full_vars:
         return object
 
     return full_vars[var]
