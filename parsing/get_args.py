f4eb5bdcb2a1e991a5d9b7c844a1bb792e691673
farkon00 <sammer2016sammer@gmail.com>
2022-05-16 18:36:21 +0300

    Octal litterals added, exception messages changed

--- parsing/get_args.py
+++ parsing/get_args.py
@@ -19,12 +19,18 @@ def get_args(lexic, state) -> Oper:
             ret.append(Oper(OpIds.value, state.current_line, [List(elems)]))
         elif var[0] == '"': 
             ret.append(Oper(OpIds.value, state.current_line, [parse_string(var, state)]))
-        elif var.lower().startswith("0x") or var.lower().startswith("-0x"):
-            binarian_assert(any(i not in "0123456789abcdef" for i in var[2:].lower()), f"Invalid hexadecimal number: {var}", state)
+        elif var.lower().startswith("0x") or var.lower().startswith("-0x"): # Hex numbers
+            binarian_assert(any(i not in "0123456789abcdef" for i in var[2:].lower()),
+             f"Invalid hexadecimal litteral: {var}", state)
             ret.append(Oper(OpIds.value, state.current_line, [int(var, 16)]))
-        elif var.lower().startswith("0b") or var.lower().startswith("-0b"):
-            binarian_assert(any(i not in "01" for i in var[2:].lower()), f"Invalid binary number: {var}", state)
+        elif var.lower().startswith("0b") or var.lower().startswith("-0b"): # Binary numbers
+            binarian_assert(any(i not in "01" for i in var[2:].lower()),
+             f"Invalid binary litteral: {var}", state)
             ret.append(Oper(OpIds.value, state.current_line, [int(var, 2)]))
+        elif var.lower().startswith("0o") or var.lower().startswith("-0b"): # Octal numbers
+            binarian_assert(any(i not in "01234567" for i in var[2:].lower()),
+             f"Invalid octal litteral: {var}", state)
+            ret.append(Oper(OpIds.value, state.current_line, [int(var, 8)]))
         elif var.isdigit() or (var[0] == "-" and var[1:].isdigit()): # int parsing
             ret.append(Oper(OpIds.value, state.current_line, int(var) if var[0] != "-" else -int(var[1:])))
         elif var.replace(".", "").isdigit() or (var[0] == "-" and var[1:].replace(".", "").isdigit()): # float parsing
